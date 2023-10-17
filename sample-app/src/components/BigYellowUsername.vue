eaf2a8ee698a2af68818b4566d70b060643a05f5
Alex Ringor <alexanderringor@gmail.com>
2023-10-17 21:37:24 +0800

    vue cli init

---
+++ sample-app/src/components/BigYellowUsername.vue
@@ -0,0 +1,23 @@
+<template>
+    <p class="username">{{ username }}</p>
+</template>
+
+<script>
+
+export default {
+  props: {
+    username: {
+      type: String,
+      required: true
+    }
+  }
+}
+
+</script>
+
+<style lang="css" scoped>
+.username {
+    font-size: 50pt;
+    color:#DCAE1D;
+}
+</style>
